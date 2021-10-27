0cc4c1164928821c0c3eaf29483d210304bb32ec
Felipe Serrou Camy de Paiva <felipeserrou@hotmail.com>
2021-10-27 00:08:07 -0400

    redirect changes

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -2,6 +2,10 @@ let bts = document.getElementsByClassName('join');
 function redirect() {
     window.open('/C:/Users/Acer/vscode/WebDev/starfleetNews/QUARK.html');
 }
+function redirectMain() {
+    location.reload();
+}
+
 function redirectArticle1() {
     window.open('/C:/Users/Acer/vscode/WebDev/starfleetNews/article-1.html')
 }
@@ -9,3 +13,4 @@ for (let bt of bts) {
     bt.addEventListener('click', redirect);
 }
 document.getElementById('article1').addEventListener('click', redirectArticle1);
+document.getElementById('starfleet').addEventListener('click', redirectMain);
