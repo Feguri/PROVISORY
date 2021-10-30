e4518e7e41171afeafe8a4146bb3cd5ba304b862
Felipe Serrou Camy de Paiva <felipeserrou@hotmail.com>
2021-10-29 23:41:49 -0400

    fixed layout bugs

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -9,8 +9,12 @@ function redirectMain() {
 function redirectArticle1() {
     window.open('/C:/Users/Acer/vscode/WebDev/starfleetNews/article-1.html')
 }
+function redirectArticle2() {
+    window.open('/C:/Users/Acer/vscode/WebDev/starfleetNews/article-2.html')
+}
 for (let bt of bts) {
     bt.addEventListener('click', redirect);
 }
 document.getElementById('article1').addEventListener('click', redirectArticle1);
+document.getElementById('article2').addEventListener('click', redirectArticle2);
 document.getElementById('starfleet').addEventListener('click', redirectMain);
